ace6b607ad050b27fab33db0298d0e70212f7f1d
Clayton <ccrenshaw2012@gmail.com>
2025-04-04 17:31:49 -0500

    setup server

---
+++ server.ts
@@ -0,0 +1,57 @@
+//to run - nodemon app.js
+
+//import stuff
+import Koa from 'koa';
+import path from 'path';
+import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
+import Router from '@koa/router';
+import dotenv from 'dotenv';
+
+//import routes
+//import bookRoutes from "./src/routes/bookRoutes";
+
+/*import Prisma client
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+// Gracefully close Prisma connection when shutting down
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  console.log("Prisma disconnected. Server shutting down.");
+  process.exit(0);
+});*/
+
+//allow environment variables
+dotenv.config();
+
+//create the app and initialize the router
+const app = new Koa();
+const router = new Router();
+
+// Define routes
+router.get('/', async (ctx) => {
+  ctx.body = 'Hello from the homepage!';
+});
+
+//Use CORS
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  allowMethods: ['GET', 'POST'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+
+//need this to parse request bodies
+//app.use(bodyParser());
+//app.use(bookRoutes.routes()).use(bookRoutes.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
+
+//ENV variables
+const PORT = process.env.PORT || 3000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
