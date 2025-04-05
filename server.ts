6ac858233dd5d51edd2924179c6c947a097a00f6
Clayton <ccrenshaw2012@gmail.com>
2025-04-04 21:35:20 -0500

    passport prototype

--- server.ts
+++ server.ts
@@ -6,7 +6,10 @@ import bodyParser from 'koa-bodyparser';
 import cors from '@koa/cors';
 import Router from '@koa/router';
 import dotenv from 'dotenv';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import crypto from 'crypto';
 
 //import routes
 //import bookRoutes from "./src/routes/bookRoutes";
@@ -47,6 +50,21 @@ app.use(cors({
 //app.use(bodyParser());
 //app.use(bookRoutes.routes()).use(bookRoutes.allowedMethods());
 app.use(router.routes()).use(router.allowedMethods());
+
+passport.use(new LocalStrategy(function verify(username, password, cb) {
+  db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
+    if (err) { return cb(err); }
+    if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+    
+    crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+      if (err) { return cb(err); }
+      if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+        return cb(null, false, { message: 'Incorrect username or password.' });
+      }
+      return cb(null, row);
+    });
+  });
+}));
 
 //ENV variables
 const PORT = process.env.PORT || 3000;
